83945b9702c9dc75bc6bfa7052eccfd215bb252b
heyile <2513931335@qq.com>
2018-11-19 16:00:45 +0800

    h2Support

--- src/main/java/com/rkd/example/Client.java
+++ src/main/java/com/rkd/example/Client.java
@@ -1,12 +1,6 @@
 package com.rkd.example;
 
-import java.util.concurrent.CountDownLatch;
-
-import com.rkd.example.metric.DefaultClientEndpointMetric;
-import com.rkd.example.metric.DefaultVertxMetricsFactory;
-
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
 import io.vertx.core.http.HttpClient;
 import io.vertx.core.http.HttpClientOptions;
 import io.vertx.core.http.HttpVersion;
@@ -14,14 +8,8 @@ import io.vertx.core.http.HttpVersion;
 public class Client {
 
   public static void main(String[] args) {
-    CountDownLatch countDownLatch = new CountDownLatch(1);
+    Vertx vertx = Vertx.vertx();
 
-    VertxOptions vertxOptions = new VertxOptions();
-    DefaultVertxMetricsFactory defaultVertxMetricsFactory = new DefaultVertxMetricsFactory();
-    vertxOptions.setMetricsOptions(defaultVertxMetricsFactory.newOptions());
-    Vertx vertx = Vertx.vertx(vertxOptions);
-
-    System.out.println("before http2:  DefaultClientEndpointMetric.INSTANCE.getAdded() is " + DefaultClientEndpointMetric.INSTANCE.getAdded());
     HttpClientOptions http2ClientOption = new HttpClientOptions();
     http2ClientOption.setProtocolVersion(HttpVersion.HTTP_2)
         .setUseAlpn(true)
@@ -35,9 +23,6 @@ public class Client {
     HttpClient localhost = http2Client.getNow(8080, "localhost", "", resp -> {
       resp.bodyHandler(buff -> {
         System.out.println(buff.toString());
-        System.out.println(
-            "after http2:  DefaultClientEndpointMetric.INSTANCE.getAdded() is " + DefaultClientEndpointMetric.INSTANCE
-                .getAdded());
       });
     });
   }
